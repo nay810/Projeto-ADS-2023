3611de31824c8eebf4563a1dfdba4e35654d6442
Rafaella Costa <rafaellamcp@gmail.com>
2023-03-28 22:16:32 -0300

    teste

---
+++ frontend/src/App.js
@@ -0,0 +1,53 @@
+import React from "react";
+import GlobalStyle from "./styles/global";
+import styled from "styled-components";
+import Form from "./componentes/Form.js";
+import Grid from "./componentes/Grid";
+import { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+
+const Title = styled.h2``;
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, setOnEdit] = useState(null);
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [setUsers]);
+
+  return (
+    <>
+      <Container>
+        <Title>CADASTRO DE FUNCIONÁRIOS</Title>
+        <Form onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers} />
+        <Grid setOnEdit={setOnEdit} users={users} setUsers={setUsers} />
+      </Container>
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
